ef1d45e3951f6c9a4275ab5b1c906b455c9f2a5c
Tobbe <tobias.bruhn@hotmail.com>
2026-04-21 11:13:06 +0200

    fix: update logo path in index.html and Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
-import logo from "../../assets/Logo.png";
+import logo from "/Logo.png";
 
 const sections = ["Home", "Projects", "About"];
 
